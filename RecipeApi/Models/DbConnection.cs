a3ca813603efc595944f2c0b675f0b031f87a74b
edknight91 <ed.knight1991@gmail.com>
2021-12-13 15:08:31 +0000

    DbConnection changes. Shelled out methods for getting recipes

--- RecipeApi/Models/DbConnection.cs
+++ RecipeApi/Models/DbConnection.cs
@@ -5,7 +5,7 @@ namespace RecipeApi.Models
     using MySqlConnector;
     public class DbConnection : IDisposable
     {
-        private const string CONNECTION_STRING = "Server=127.0.0.1;Database=myshop;Uid=root;Pwd=Developer!;";
+        private const string CONNECTION_STRING = "Server=127.0.0.1;Database=recipeapi;Uid=root;Pwd=Developer!;";
 
         public MySqlConnection Connection { get; }
 
@@ -16,6 +16,44 @@ namespace RecipeApi.Models
             Connection = new MySqlConnection(CONNECTION_STRING);
             Connection.Open();
         }
+
+        public DataTable GetAllRecipes()
+        {
+            MySqlCommand command = this.Connection.CreateCommand();
+            command.CommandText = "SELECT * FROM recipe;";
+            //            command.Exec
+
+            DataTable t = new DataTable();
+
+            MySqlDataAdapter x = new MySqlDataAdapter(command);
+            x.Fill(t);
+
+            return t;
+        }
+
+        public void CreateRecipe(string title, string method)
+        {
+            MySqlCommand command = this.Connection.CreateCommand();
+            command.CommandText = "INSERT INTO recipe (title, method) VALUES (@title, @method);";
+
+            command.Parameters.Add(new MySqlParameter("@title", title));
+            command.Parameters.Add(new MySqlParameter("@method", method));
+
+            command.ExecuteNonQuery();
+
+        }
+
+        public void UpdateRecipe(string title, string method)
+        {
+            MySqlCommand command = this.Connection.CreateCommand();
+            command.CommandText = "UPDATE recipe SET title = @title, method = @method";
+
+            command.Parameters.Add(new MySqlParameter("@title", title));
+            command.Parameters.Add(new MySqlParameter("@method", method));
+
+            command.ExecuteNonQuery();
+        }
+
 
         public void Dispose() => Connection.Dispose();
     }
